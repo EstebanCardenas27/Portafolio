4f1f3b49518b72d3b0d11209886deb46148aea44
Estebancardenas27 <estebancardenas2796@gmail.com>
2026-08-04 12:54:56 -0400

    feat: add basename

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -9,7 +9,7 @@ import ScrollToTop from "@/components/ScrollToTop/ScrollToTop";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Portafolio">
     <ScrollToTop/>
       <Routes>        
         <Route path="/" element = {<Layout />}>          
